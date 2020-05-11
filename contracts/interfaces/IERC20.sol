d6448c47e5f87f6842f85af9015cf309eca84c39
Robsonsjre <robsonsjre@gmail.com>
2020-05-10 21:15:20 -0300

    added interfaces folder

---
+++ contracts/interfaces/IERC20.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.16;
+
+interface IERC20 {
+    function approve(address, uint) external;
+    function transfer(address, uint) external;
+    function transferFrom(address, address, uint) external;
+    function deposit() external payable;
+    function withdraw(uint) external;
+    function balanceOf(address) external view returns (uint);
+}
